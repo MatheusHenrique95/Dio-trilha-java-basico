2c742d889b854e3cf2f9df0a53535a147c311a0f
Matheus Henrique <mhssantana95@gmail.com>
2024-05-13 21:11:17 -0300

    ContarProjeto

---
+++ DesafioControleFluxo/src/Contador.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = scanner.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = scanner.nextInt();
+		
+		try {
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+			System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if (parametroUm>parametroDois) {
+            throw new ParametrosInvalidosException();            		
+        }
+		int contagem = parametroDois - parametroUm;
+        for(int imprim = 1; imprim<=contagem;imprim++){
+            System.out.println("Imprimindo o número "+imprim);
+        }
+		
+	}
+
+}
